68bb12f4ce7dbd211c93e46756d5979ae33255e3
Pol Bonastre <pbr1111@gmail.com>
2018-07-12 23:25:05 +0200

    Fix login styles and contacts page

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -18,20 +18,18 @@ export class ContactsPage {
     private loadContactsFromDevice(): void {
         this.page.showLoading();
         this.contacts.find(['displayName', 'photos', 'phoneNumbers'], { multiple: true })
-            .then(this.onLoadContactsCompleted);
+            .then(contactList => this.onLoadContactsCompleted(contactList));
     }
 
     private onLoadContactsCompleted(contactsList: Array<Contact>): void {
         if (contactsList != null) {
             for (let contact of contactsList) {
-                if (contact.displayName != null) {
-                    let phone = contact.phoneNumbers != null && contact.phoneNumbers.length > 0 ?
-                        contact.phoneNumbers[0].value
-                        : null;
-                    let photoUrl: string | SafeValue = "assets/imgs/user.svg";
+                if (contact.displayName != null && contact.phoneNumbers != null && contact.phoneNumbers.length > 0) {
+                    let phone = contact.phoneNumbers[0].value;
+                    let photoUrl: string | SafeValue = "assets/imgs/user.png";
                     if (contact.photos != null && contact.photos.length > 0) {
                         photoUrl = this.sanitizer.bypassSecurityTrustUrl(contact.photos[0].value);
-                    } 
+                    }
                     this.userContacts.push({
                         displayName: contact.displayName,
                         photoUrl: photoUrl,
@@ -39,6 +37,17 @@ export class ContactsPage {
                     });
                 }
             }
+        }
+        if (this.userContacts.length > 0) {
+            this.userContacts = this.userContacts.sort((contact1, contact2) => {
+                if (contact1.displayName > contact2.displayName) {
+                    return 1;
+                }
+                if (contact1.displayName < contact2.displayName) {
+                    return -1;
+                }
+                return 0;
+            });
         }
         this.page.hideLoading();
     }
--- src/shared/services/page.service.ts
+++ src/shared/services/page.service.ts
@@ -2,6 +2,8 @@ import { LocaleService } from './locale.service';
 import { AlertController, LoadingController, NavController, Loading, App } from 'ionic-angular';
 import { Injectable } from "@angular/core";
 
+const minLoadingMs: number = 1000;
+
 @Injectable()
 export class PageService {
     loading: Loading;
@@ -15,7 +17,7 @@ export class PageService {
         this.navCtrl = app.getActiveNav();
     }
 
-    public showLoading(dissmissOnPageChange: boolean = true): void {
+    public showLoading(dissmissOnPageChange: boolean = false): void {
         this.loading = this.loadingCtrl.create({
             content: this.locale.getTranslation("pleaseWait"),
             dismissOnPageChange: dissmissOnPageChange
@@ -24,7 +26,7 @@ export class PageService {
     }
 
     public hideLoading(): void {
-        this.loading.dismiss();
+        setTimeout(() => this.loading.dismissAll(), minLoadingMs);
     }
 
     public showError(text: string) {
